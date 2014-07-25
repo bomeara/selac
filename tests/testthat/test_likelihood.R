0d06fa1d64c0133bbcacb8e27f655be2ca821081
Brian OMeara <omeara.brian@gmail.com>
2014-07-25 17:09:16 -0400

    conversion of functions, starting to test likelihood

---
+++ tests/testthat/test_likelihood.R
@@ -0,0 +1,10 @@
+test_that("GetLikelihoodSAC_CodonForSingleCharGivenOptimum", {
+  ref <- c("U15717", "U15718", "U15719", "U15720",
+         "U15721", "U15722", "U15723", "U15724")
+  Rampho <- read.GenBank(ref)
+  chars <- DNAbinToCodonNumeric(Rampho)
+  tree <- rcoal(n=dim(chars)[1], tip.label=chars[,1])
+  Q_codon <- CreateCodonFixationRateMatrix(aa_op="M", s=2, aa.distances=CreateAADistanceMatrix())
+  likelihood <- GetLikelihoodSAC_CodonForSingleCharGivenOptimum(codon.data=chars, phy=tree, Q_codon=Q_codon)
+  expect_is(likelihood, "numeric")
+})
