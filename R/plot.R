8b1ad6cf52a2e2c5081327e2f390806dd3cf0df6
Brian OMeara <omeara.brian@gmail.com>
2016-11-08 17:03:38 -0500

    plotting code works, but now need to add labels and color

--- R/plot.R
+++ R/plot.R
@@ -1,7 +1,7 @@
 #' Function to plot frequency of distribution of different Wi given selac parameters
 
-ComputeEquilibriumFrequencies <- function(nuc.model="JC", base.freqs=rep(0.25, 4), nsites=1, C=4, Phi=0.5, q=4e-7, Ne=5e6, alpha=1.83, beta=0.10, gamma=0.0003990333, volume.fixed.value=0003990333, include.stop.codon=TRUE, numcode=1, diploid=TRUE, flee.stop.codon.rate=0.9999999) {
-#To test: nuc.model="JC"; base.freqs=rep(0.25, 4); nsites=1; C=4; Phi=0.5; q=4e-7; Ne=5e6; alpha=1.83; beta=0.10; gamma=0.0003990333; volume.fixed.value=0003990333; include.stop.codon=TRUE; numcode=1; diploid=TRUE; flee.stop.codon.rate=0.9999999
+ComputeEquilibriumFrequencies <- function(nuc.model="JC", base.freqs=rep(0.25, 4), nsites=1, C=4, Phi=0.5, q=4e-7, Ne=5e6, alpha=1.83, beta=0.10, gamma=0.0003990333, include.stop.codon=TRUE, numcode=1, diploid=TRUE, flee.stop.codon.rate=0.9999999) {
+#To test: nuc.model="JC"; base.freqs=rep(0.25, 4); nsites=1; C=4; Phi=0.5; q=4e-7; Ne=5e6; alpha=1.83; beta=0.10; gamma=0.0003990333; include.stop.codon=TRUE; numcode=1; diploid=TRUE; flee.stop.codon.rate=0.9999999
   nuc.mutation.rates <- selac:::CreateNucleotideMutationMatrix(1, model=nuc.model, base.freqs=base.freqs)
   codon.index.matrix = selac:::CreateCodonMutationMatrixIndex()
   codon_mutation_matrix <- matrix(nuc.mutation.rates[codon.index.matrix], dim(codon.index.matrix))
@@ -32,85 +32,34 @@ ComputeEquilibriumFrequencies <- function(nuc.model="JC", base.freqs=rep(0.25, 4
   rownames(eq.freq.matrix) <- rownames(Q_codon_array)
   colnames(eq.freq.matrix) <- .nonstop.aa
   for (i in sequence(length(.nonstop.aa))) {
-    eq.freq.matrix[,i] <- expm(Q_codon_array[,,.nonstop.aa[i]] * 1000000, method=c("Ward77")) %*% liks[1,]
+    eq.freq.matrix[,i] <- expm::expm(t(Q_codon_array[,,.nonstop.aa[i]]) * 1000000, method=c("Ward77")) %*% liks[1,]
   }
   return(eq.freq.matrix)
-  #fitnesses <- GetFitness(focal.protein, optimal.protein, aa.distances, nsites, C, Phi, q, Ne, diploid)
 }
 
-
-
-# CODE FROM JEREMY
-
-
-#
-# library(selac)
-# library(expm)
-#
-# rates <- c()
-# base.freqs <- rep(.25,4)
-# nuc.model <- "JC"
-# nsites <- 400
-# diploid <- FALSE
-# numcode <- 1
-#
-# #Assumes JC rates:
-# input.pars <- c(2, 1.83, .10)
-#
-# ###############
-# C.Phi.q.Ne <- input.pars[1]
-# C <- 4
-# q <- 4e-7
-# Ne <- 5e6
-# Phi.q.Ne <- C.Phi.q.Ne / C
-# Phi.Ne <- Phi.q.Ne / q
-# Phi <- Phi.Ne / Ne
-# alpha <- input.pars[2]
-# beta <- input.pars[3]
-# gamma <- volume.fixed.value <- 0.0003990333
-#
-#
-# #Step 1: Get nucleotide rates:
-#
-# nuc.mutation.rates <- selac:::CreateNucleotideMutationMatrix(1, model=nuc.model, base.freqs=base.freqs)
-#
-# codon.index.matrix = selac:::CreateCodonMutationMatrixIndex()
-# codon_mutation_matrix <- matrix(nuc.mutation.rates[codon.index.matrix], dim(codon.index.matrix))
-# codon_mutation_matrix[is.na(codon_mutation_matrix)]=0
-#
-# aa.distances <- selac:::CreateAADistanceMatrix(alpha=alpha, beta=beta, gamma=gamma, aa.properties=NULL, normalize=FALSE, poly.params=NULL, k=0)
-# Q_codon_array <- selac:::FastCreateAllCodonFixationProbabilityMatrices(aa.distances=aa.distances, nsites=nsites, C=C, Phi=Phi, q=q, Ne=Ne, include.stop.codon=TRUE, numcode=numcode, diploid=diploid, flee.stop.codon.rate=0.9999999)
-#
-# diag(codon_mutation_matrix) = 0
-# diag(codon_mutation_matrix) = -rowSums(codon_mutation_matrix)
-#
-# ###### Not sure these lines are necessary ########
-# #scale.factor <- -sum(diag(codon_mutation_matrix) * codon.freq.by.gene, na.rm=TRUE)
-# #codon_mutation_matrix_scaled = codon_mutation_matrix * (1/scale.factor)
-# codon_mutation_matrix_scaled <- codon_mutation_matrix
-# ##################################################
-#
-# .unique.aa <- c("K", "N", "T", "R", "S", "I", "M", "Q", "H", "P", "L", "E", "D", "A", "G", "V", "*", "Y", "C", "W", "F")
-#
-# #Finish the Q_array codon mutation matrix multiplication here:
-# for(k in 1:21){
-#     if(diploid == TRUE){
-#         Q_codon_array[,,.unique.aa[k]] = (2 * Ne) * codon_mutation_matrix_scaled * Q_codon_array[,,.unique.aa[k]]
-#     }else{
-#         Q_codon_array[,,.unique.aa[k]] = Ne * codon_mutation_matrix_scaled * Q_codon_array[,,.unique.aa[k]]
-#     }
-#     diag(Q_codon_array[,,.unique.aa[k]]) = 0
-#     diag(Q_codon_array[,,.unique.aa[k]]) = -rowSums(Q_codon_array[,,.unique.aa[k]])
-# }
-#
-# starting.state <- 1
-# liks <- matrix(0, 1, 64)
-# liks[,starting.state] <- 1
-# eq.freq <- expm(Q_codon_array[,,optimal.aa] * 1000000, method=c("Ward77")) %*% liks[1,]
-#
-#
-#
-#
-#
-#
-#
+#' Function to plot a distribution of frequencies of codons given a 3d array of equilibrium frequency matrices
+#'
+#' @param eq.freq.matrices A 3d array of eq.freq.matrix returned from ComputeEquilibriumFrequencies
+#' @param values The vector of labels for each matrix (i.e., different Phi values)
+#' @example
+#' phi.vector <- c(0.01, 0.5, 10)
+#' eq.freq.matrices <- array(dim=c(64, 20, 3))
+#' for (i in sequence(length(phi.vector))) {
+#'   eq.freq.matrices[,,i] <- ComputeEquilibriumFrequencies(Phi=phi.vector[i])
+#' }
+#' values = paste("Phi = ", phi.vector, sep="")
+#' PlotEquilbriumDistribution(eq.freq.matrices, values)
+PlotEquilbriumDistribution <- function(eq.freq.matrices, values) {
+  library(RColorBrewer) #MOVE TO NAMESPACE
+  distributions <- list()
+  total.range <- c(NA)
+  for (i in sequence(dim(eq.freq.matrices)[3])) {
+    sorted <- apply(eq.freq.matrices[,,i], 2, sort, decreasing=TRUE)
+    distributions[[i]] <- apply(sorted, 1, mean)
+    total.range <- range(c(distributions[i], total.range), na.rm=TRUE)
+  }
+  plot(x=c(1,64), y=total.range, type="n", bty="n", xlab="index", ylab="Average frequency")
+  for (i in sequence(dim(eq.freq.matrices)[3])) {
+    lines(sequence(length(distributions[[i]])), distributions[[i]])
+  }
+}
