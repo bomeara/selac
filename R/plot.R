cc913336d62f145793b63c092446188de118ded8
Brian OMeara <omeara.brian@gmail.com>
2016-11-08 16:24:19 -0500

    getting equilibrium freqs

--- R/plot.R
+++ R/plot.R
@@ -1,12 +1,41 @@
 #' Function to plot frequency of distribution of different Wi given selac parameters
 
-PlotFrequencySpace <- function(aa.distances=CreateAADistanceMatrix(), nsites=1, C=4, Phi=0.5, q=4e-7, Ne=5e6, include.stop.codon=TRUE, numcode=1, diploid=TRUE, flee.stop.codon.rate=0.9999999) {
+ComputeEquilibriumFrequencies <- function(nuc.model="JC", base.freqs=rep(0.25, 4), nsites=1, C=4, Phi=0.5, q=4e-7, Ne=5e6, alpha=1.83, beta=0.10, gamma=0.0003990333, volume.fixed.value=0003990333, include.stop.codon=TRUE, numcode=1, diploid=TRUE, flee.stop.codon.rate=0.9999999) {
+#To test: nuc.model="JC"; base.freqs=rep(0.25, 4); nsites=1; C=4; Phi=0.5; q=4e-7; Ne=5e6; alpha=1.83; beta=0.10; gamma=0.0003990333; volume.fixed.value=0003990333; include.stop.codon=TRUE; numcode=1; diploid=TRUE; flee.stop.codon.rate=0.9999999
+  nuc.mutation.rates <- selac:::CreateNucleotideMutationMatrix(1, model=nuc.model, base.freqs=base.freqs)
+  codon.index.matrix = selac:::CreateCodonMutationMatrixIndex()
+  codon_mutation_matrix <- matrix(nuc.mutation.rates[codon.index.matrix], dim(codon.index.matrix))
+  codon_mutation_matrix[is.na(codon_mutation_matrix)]=0
+  aa.distances <- selac:::CreateAADistanceMatrix(alpha=alpha, beta=beta, gamma=gamma, aa.properties=NULL, normalize=FALSE, poly.params=NULL, k=0)
+  Q_codon_array <- selac:::FastCreateAllCodonFixationProbabilityMatrices(aa.distances=aa.distances, nsites=nsites, C=C, Phi=Phi, q=q, Ne=Ne, include.stop.codon=TRUE, numcode=numcode, diploid=diploid, flee.stop.codon.rate=0.9999999)
+  diag(codon_mutation_matrix) = 0
+  diag(codon_mutation_matrix) = -rowSums(codon_mutation_matrix)
+  .unique.aa <- c("K", "N", "T", "R", "S", "I", "M", "Q", "H", "P", "L", "E", "D", "A", "G", "V", "*", "Y", "C", "W", "F")
 
+  #Finish the Q_array codon mutation matrix multiplication here:
+  for(k in 1:21){
+      if(diploid == TRUE){
+          Q_codon_array[,,.unique.aa[k]] = (2 * Ne) * codon_mutation_matrix * Q_codon_array[,,.unique.aa[k]]
+      }else{
+          Q_codon_array[,,.unique.aa[k]] = Ne * codon_mutation_matrix * Q_codon_array[,,.unique.aa[k]]
+      }
+      diag(Q_codon_array[,,.unique.aa[k]]) = 0
+      diag(Q_codon_array[,,.unique.aa[k]]) = -rowSums(Q_codon_array[,,.unique.aa[k]])
+  }
 
-  codon.fixation.prob <- FastCreateAllCodonFixationProbabilityMatrices(aa.distances, nsites, C, Phi, q, Ne, include.stop.codon, numcode, diploid, flee.stop.codon.rate)
-  equilibrium.codon.freq <-
-
-  fitnesses <- GetFitness(focal.protein, optimal.protein, aa.distances, nsites, C, Phi, q, Ne, diploid)
+  starting.state <- 1
+  liks <- matrix(1/64, 1, 64)
+  #liks[,starting.state] <- 1
+  .nonstop.aa <- .unique.aa[-which(.unique.aa=="*")]
+  #eq.freq <- expm(Q_codon_array[,,optimal.aa] * 1000000, method=c("Ward77")) %*% liks[1,]
+  eq.freq.matrix <- matrix(nrow=64, ncol=length(.nonstop.aa))
+  rownames(eq.freq.matrix) <- rownames(Q_codon_array)
+  colnames(eq.freq.matrix) <- .nonstop.aa
+  for (i in sequence(length(.nonstop.aa))) {
+    eq.freq.matrix[,i] <- expm(Q_codon_array[,,.nonstop.aa[i]] * 1000000, method=c("Ward77")) %*% liks[1,]
+  }
+  return(eq.freq.matrix)
+  #fitnesses <- GetFitness(focal.protein, optimal.protein, aa.distances, nsites, C, Phi, q, Ne, diploid)
 }
 
 
@@ -14,7 +43,7 @@ PlotFrequencySpace <- function(aa.distances=CreateAADistanceMatrix(), nsites=1, 
 # CODE FROM JEREMY
 
 
-# 
+#
 # library(selac)
 # library(expm)
 #
