cfefb805ea673a18ee7a15d6d752e1d9d2d0090f
Michael Gilchrist <mikeg@utk.edu>
2017-04-27 16:28:42 -0400

    creating files for testing ODE solver performance. Also learning more about git

---
+++ ode.solver.evaluation/testdeq.R
@@ -0,0 +1,85 @@
+## header taken from selac.R
+###LOAD REQUIRED PACKAGES -- eventually move to namespace:
+library(ape)
+library(expm)
+library(nnet)
+library(nloptr)
+library(seqinr)
+library(phangorn)
+library(MASS)
+library(parallel)
+library(Rcpp)
+library(RcppArmadillo)
+library(inline)
+##need to load deSolve before dyn.load(*.so)
+library(deSolve)
+
+## don't need to load selac package
+## load compiled c code
+## May need to recompile if source file has changed.
+## To do so go to src/ and enter
+##      R CMD SHLIB selacHMM.c
+## only need to do this once
+## We're doing this because we're not
+## loading the selac package
+dyn.load("../../src/selacHMM.so") 
+
+
+##sourcing selac.R
+source("../../R/selac.R")
+
+
+
+##Load dataset
+tree <- read.tree("rokasYeast.tre") ##in directory testthat/ which should be the working dir
+phy <- drop.tip(tree, "Calb")
+##reset branch length of phy$edge.length[1]=12 for testing long branch lengths.
+yeast.gene <- read.dna("gene1Yeast.fasta", format="fasta")
+yeast.gene <- as.list(as.matrix(cbind(yeast.gene))[1:7,])
+chars <- DNAbinToCodonNumeric(yeast.gene)
+codon.data <- chars[phy$tip.label,]
+aa.data <- ConvertCodonNumericDataToAAData(codon.data, numcode=1)
+aa.optim <- apply(aa.data[, -1], 2, GetMaxName) #starting values for all, final values for majrule
+aa.optim.full.list <- aa.optim
+codon.freq.by.aa <- GetCodonFreqsByAA(codon.data[,-1], aa.optim, numcode=1)
+codon.freq.by.gene <- GetCodonFreqsByGeneHMM(codon.data[,-1])
+aa.optim.frame.to.add <- matrix(c("optimal", aa.optim), 1, dim(codon.data)[2])
+colnames(aa.optim.frame.to.add) <- colnames(codon.data)
+codon.data <- rbind(codon.data, aa.optim.frame.to.add)
+codon.data <- SitePattern(codon.data, includes.optimal.aa=TRUE)
+aa.optim = codon.data$optimal.aa
+codon.index.matrix <- CreateCodonMutationMatrixIndexEvolveAA()
+
+
+##source my modified files
+source("../treeTraversalODETests.R")
+
+
+###Calc likelihood using standard set up
+
+runDiagnostics=TRUE; ## for ode solver output
+## from ode help page
+if(FALSE){
+    odeMethodVec = c("lsoda", "lsode", "lsodes", "lsodar", "vode", "daspk",
+                "euler", "rk4", "ode23", "ode45", "radau", 
+                "bdf", "bdf_d", "adams", "impAdams", "impAdams_d", "iteration")
+    }else{
+        odeMethodVec = c("lsoda")
+    }
+
+for(odeMethod in odeMethodVec){
+
+    ##WARNING: RUNNIGN THIS FROM WITHIN EMACS SEEMS TO NOT ALWAYS WORK PROPERLY
+    ##TRY CUTTING AND PASTING OR SOURCING
+    
+    runTime = system.time(
+        selac.unrest.evolveAA <- GetLikelihoodSAC_CodonForManyCharGivenAllParamsEvolvingAA(log(c(4*4e-7*.5*5e6, 1.829272, 0.101799, rep(1,11), 0.01)), codon.data, phy, codon.freq.by.aa=codon.freq.by.aa, codon.freq.by.gene=codon.freq.by.gene, numcode=1, diploid=TRUE, aa.properties=NULL, volume.fixed.value=0.0003990333, nuc.model="UNREST", codon.index.matrix, include.gamma=FALSE, ncats=4, k.levels=0, logspace=TRUE, verbose=FALSE, n.cores.by.gene.by.site=1) #n.cores.by.gene?
+    )
+    roundLLik <- round(selac.unrest.evolveAA, 3);
+
+    print(paste("Run time: ", runTime, " LLik for ", odeMethod, " with settings" , ": ", roundLLik, " \t deviation:", -8677.442 - roundLLik , sep=""))
+    comparison <- identical(round(selac.unrest.evolveAA, 3), -8677.442) ##Correct LLik value seen using lsoda and matrix exponentiation
+    }
+
+ 
+
---
+++ ode.solver.evaluation/treeTraversalODETests.R
@@ -0,0 +1,78 @@
+###Modified version of function taken 4/25/17 from selac.R in git repository
+## Changing to explore different ODE solvers in hopes of speeding things up.
+
+TreeTraversalODE <- function(phy, Q_codon_array_vectored, liks.HMM, bad.likelihood=-100000, root.p) {
+    if(runDiagnostics) print("site: Unknown")
+    nb.tip <- length(phy$tip.label)
+    nb.node <- phy$Nnode
+
+    anc <- unique(phy$edge[,1])
+    TIPS <- 1:nb.tip
+    comp <- numeric(nb.tip + nb.node)
+    #Start the postorder traversal indexing lists by node number:
+    for (i in seq(from = 1, length.out = nb.node)) {
+        focal <- anc[i]
+        desRows <- which(phy$edge[,1]==focal)
+        desNodes <- phy$edge[desRows,2]
+        v = rep(1, dim(liks.HMM)[2])
+        if(runDiagnostics) print(paste("Node: ", i, " out of ", nb.node, "\n")) 
+        for (desIndex in sequence(length(desRows))){
+            if(runDiagnostics) print(paste("descendent: ", desIndex, " out of ", length(desRows), "\n")) 
+            yini <- liks.HMM[desNodes[desIndex],]
+            times=c(0, phy$edge.length[desRows[desIndex]])
+	    ##Change ODE solver here
+            ## swapped lsoda() for ode() so we can define solver as an argument to function.
+            ## Note to access all of the rk functions you can use
+            ##         method = rkMethod("rk45dp7", densetype = NULL, nknots = 5)
+            ##
+            prob.subtree.cal.full <- ode(y=yini, times=times, func = "selacHMM", parms=Q_codon_array_vectored, initfunc="initmod_selacHMM", dllname = "selac", method=odeMethod, hini=0)
+            ## options for runge-kutta rk()
+            ##rk(y, times, func, parms, rtol = 1e-6, atol = 1e-6,
+            ##verbose = FALSE, tcrit = NULL, hmin = 0, hmax = NULL,
+            ##hini = hmax, ynames = TRUE, method = rkMethod("rk45dp7", ... ),
+            ##maxsteps = 5000, dllname = NULL, initfunc = dllname,
+            ##initpar = parms, rpar = NULL, ipar = NULL,
+            ##nout = 0, outnames = NULL, forcings = NULL,
+            ##initforc = NULL, fcontrol = NULL, events = NULL, ...)
+            ##
+            ## method : the integrator to use. This can either be a string constant
+            ##naming one of the pre-defined methods or a call to function
+            ##‘rkMethod’ specifying a user-defined method.  The most common
+            ##methods are the fixed-step methods ‘"euler"’, second and
+            ##fourth-order Runge Kutta (‘"rk2"’, ‘"rk4"’), or the variable
+            ##step methods Bogacki-Shampine ‘"rk23bs"’,
+            ##Runge-Kutta-Fehlberg ‘"rk34f"’, the fifth-order Cash-Karp
+            ##method ‘"rk45ck"’ or the fifth-order Dormand-Prince method
+            ##with seven stages ‘"rk45dp7"’.  As a suggestion, one may use
+            ##‘"rk23bs"’ (alias ‘"ode23"’) for simple problems and
+            ##‘"rk45dp7"’ (alias ‘"ode45"’) for rough problems.
+
+            if(runDiagnostics){
+                print("Running diagnostics\n")
+                diagnostics(prob.subtree.cal.full)
+                
+            } 
+            ######## THIS CHECKS TO ENSURE THAT THE INTEGRATION WAS SUCCESSFUL ###########
+            if(attributes(prob.subtree.cal.full)$istate[1] < 0){
+                return(bad.likelihood)
+            }else{
+                prob.subtree.cal <- prob.subtree.cal.full[-1,-1]
+            }
+            ##############################################################################
+
+            if(prob.subtree.cal[1]<0){
+                return(bad.likelihood)
+            }
+            v <- v * prob.subtree.cal
+        }
+        comp[focal] <- sum(v)
+        liks.HMM[focal,] <- v/comp[focal]
+    }
+    root.node <- nb.tip + 1L
+    if (is.na(sum(log(liks.HMM[root.node,])))){
+        return(bad.likelihood)
+    }else{
+        loglik <- -(sum(log(comp[-TIPS])) + log(sum(root.p * liks.HMM[root.node,])))
+    }
+    return(loglik)
+}
