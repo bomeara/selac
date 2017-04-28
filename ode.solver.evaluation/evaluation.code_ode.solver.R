72273e6642b23dc431e88ba2d03328cf828e6270
Michael Gilchrist <mikeg@utk.edu>
2017-04-28 13:01:39 -0400

    code can now be set to run on a single branch, which speeds up things considerably.  Setting hini=0.01 results in ode45 working and is an order of magnitude faster that lsoda wtih the same settings.

--- ode.solver.evaluation/evaluation.code_ode.solver.R
+++ ode.solver.evaluation/evaluation.code_ode.solver.R
@@ -38,17 +38,49 @@ if(FALSE){
 }
 
 
+##testing settings
+tree.used <- "branch" #branch with 1 species or full tree with 6 species
+branch.type="long"
+hini.vec <- c(0, 0.01, .1, 1, first.branch.length/100, first.branch.length/10, first.branch.length/2) 
+my.hini <-  hini.vec[2];
+runDiagnostics=TRUE; ## for ode solver output
+
+## methods from ode help page
+if(FALSE){
+    odeMethodVec = c("lsoda", "lsode", "lsodes", "lsodar", "vode", "daspk",
+                "euler", "rk4", "ode23", "ode45", "radau", 
+                "bdf", "bdf_d", "adams", "impAdams", "impAdams_d", "iteration")
+    }else{
+        odeMethodVec = c("lsoda")
+    }
+
+
+
 ##Load dataset
 tree <- read.tree("rokasYeast.tre") ##in directory testthat/ which should be the working dir
-##reference phylogenetic tree
-ref.phy <- drop.tip(tree, "Calb") ##try dropping all tips but one?
-##modified version of ref.phy for testing long branch length calculations.
-long.phy <- ref.phy
+species.vec <- tree$tip.label
+
+##define short.phy tree as either branch or full tree
+switch(tree.used,
+      "full"={
+          short.phy <- drop.tip(tree, "Calb") ##try dropping all tips but one
+          expected.LLik.list <- list(short = -8677.442, long = -9017.141)
+          },
+      "branch"={
+          short.phy <- drop.tip(tree, species.vec[-1]) ##Drop all tips but first
+          expected.LLik.list <- list(short = -2211.094, long = -2343.024)
+      },
+      warning(paste("tree.used not properly defined: ", tree.used, "must be either 'full' or 'branch'"))
+      )
+
+
+##modified version of short.phy for testing long branch length calculations.
+long.phy <- short.phy
 long.phy$edge.length[1]=12
 yeast.gene <- read.dna("gene1Yeast.fasta", format="fasta")
 yeast.gene <- as.list(as.matrix(cbind(yeast.gene))[1:7,])
 chars <- DNAbinToCodonNumeric(yeast.gene)
-codon.data <- chars[ref.phy$tip.label,]
+codon.data <- chars[short.phy$tip.label,]
 aa.data <- ConvertCodonNumericDataToAAData(codon.data, numcode=1)
 aa.optim <- apply(aa.data[, -1], 2, GetMaxName) #starting values for all, final values for majrule
 aa.optim.full.list <- aa.optim
@@ -61,32 +93,19 @@ codon.data <- SitePattern(codon.data, includes.optimal.aa=TRUE)
 aa.optim = codon.data$optimal.aa
 codon.index.matrix <- CreateCodonMutationMatrixIndexEvolveAA()
 
-phy.list <- list(ref=ref.phy, long=long.phy)
-expected.LLik.list <- list(ref = -8677.442, long = -9017.141)
+phy.list <- list(short=short.phy, long=long.phy)
 
-runDiagnostics=FALSE; ## for ode solver output
+
 
 
 ###Calc likelihood using standard set up
 
-## methods from ode help page
-if(FALSE){
-    odeMethodVec = c("lsoda", "lsode", "lsodes", "lsodar", "vode", "daspk",
-                "euler", "rk4", "ode23", "ode45", "radau", 
-                "bdf", "bdf_d", "adams", "impAdams", "impAdams_d", "iteration")
-    }else{
-        odeMethodVec = c("ode45")
-    }
 
 ##source my modified files
 source("./treeTraversalODETests.R")
 
-##testing settings
-phy.used="long"
 
-first.branch.length <- phy.list[[phy.used]]$edge.length[1]
-hini.vec <- c(0, 0.01, .1, 1, first.branch.length/100, first.branch.length/10, first.branch.length/2) 
-my.hini <-  hini.vec[1];
+first.branch.length <- phy.list[[branch.type]]$edge.length[1]
 
 ##model parameters
 cPhiqNe <- 4*4e-7*.5*5e6
@@ -100,17 +119,15 @@ params <- c(cPhiqNe, grantham.alpha, grantham.beta , unrest.mutation.rates, rate
 for(odeMethod in odeMethodVec){
 
     print(paste("evaluating method: ", odeMethod))
-    ##WARNING: RUNNIGN THIS FROM WITHIN EMACS SEEMS TO NOT ALWAYS WORK PROPERLY
-    ##TRY CUTTING AND PASTING OR SOURCING
 
     ##GetLikelihoodSAC_... ultimately calls TreeTraversalODE which we are tweaking here
     ## It is defined in treeTraversalODETests.R
     runTime = system.time(
-        selac.unrest.evolveAA <- GetLikelihoodSAC_CodonForManyCharGivenAllParamsEvolvingAA(log(params), codon.data, phy.list[[phy.used]], codon.freq.by.aa=codon.freq.by.aa, codon.freq.by.gene=codon.freq.by.gene, numcode=1, diploid=TRUE, aa.properties=NULL, volume.fixed.value=0.0003990333, nuc.model="UNREST", codon.index.matrix, include.gamma=FALSE, ncats=4, k.levels=0, logspace=TRUE, verbose=FALSE, n.cores.by.gene.by.site=1) 
+        selac.unrest.evolveAA <- GetLikelihoodSAC_CodonForManyCharGivenAllParamsEvolvingAA(log(params), codon.data, phy.list[[branch.type]], codon.freq.by.aa=codon.freq.by.aa, codon.freq.by.gene=codon.freq.by.gene, numcode=1, diploid=TRUE, aa.properties=NULL, volume.fixed.value=0.0003990333, nuc.model="UNREST", codon.index.matrix, include.gamma=FALSE, ncats=4, k.levels=0, logspace=TRUE, verbose=FALSE, n.cores.by.gene.by.site=1) 
     )
     roundLLik <- round(selac.unrest.evolveAA, 3);
 
-    print(paste("CPU time: ", runTime[1], ", Method: ", odeMethod, ", phy.used:" , phy.used, "hini: ", my.hini, ", LLik: ", roundLLik, ", deviation:", expected.LLik.list[phy.used] - roundLLik , sep=""))
+    print(paste("CPU time: ", runTime[1], ", Method: ", odeMethod, ", tree.used: ", tree.used, ", branch.type: " , branch.type, ", hini: ", my.hini, ", LLik: ", roundLLik, ", deviation:", expected.LLik.list[[branch.type]] - roundLLik , sep=""))
     comparison <- identical(round(selac.unrest.evolveAA, 3), -8677.442) ##Correct LLik value seen using lsoda and matrix exponentiation
     }
 
