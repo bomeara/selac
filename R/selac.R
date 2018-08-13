4ea43565be9e1a03db5dfa0ac46c34a757cbd2b0
Graham Derryberry <g.derryberry@gmail.com>
2018-08-13 13:27:03 -0400

    Internalizing expAtv method from expm package

--- R/selac.R
+++ R/selac.R
@@ -1117,10 +1117,10 @@ GetLikelihoodSAC_CodonForSingleCharGivenOptimumHMMScoring <- function(charnum=1,
     for (rowIndex in desRows){
       if(phy$edge[rowIndex,2] <= nb.tip){
         if(sum(liks.HMM[phy$edge[rowIndex,2],]) < 65){
-          v <- v * expm::expAtv(A=Q_codon_array ,t=phy$edge.length[rowIndex], v=liks.HMM[phy$edge[rowIndex,2],])$eAtv
+          v <- v * internal_expAtv(A=Q_codon_array ,t=phy$edge.length[rowIndex], v=liks.HMM[phy$edge[rowIndex,2],])
         }
       }else{
-        v <- v * expm::expAtv(A=Q_codon_array ,t=phy$edge.length[rowIndex], v=liks.HMM[phy$edge[rowIndex,2],])$eAtv
+        v <- v * internal_expAtv(A=Q_codon_array ,t=phy$edge.length[rowIndex], v=liks.HMM[phy$edge[rowIndex,2],])
       }
     }
     comp[focal] <- sum(v)
@@ -3505,6 +3505,139 @@ internal_expmt <- function (A, t_vec) {
   return(res)
 }
 
+## This is a editted copy from the expm package; will be replaced with HMM variants for fixed values
+internal_expAtv <- function(A, v, t=1)
+{
+  #Hardcoded arguments
+  tol=1e-7; btol = 1e-7; m.max = 30; mxrej = 10 #constant
+  ## R translation:  Ravi Varadhan, Johns Hopkins University
+  ##		   "cosmetic", apply to sparse A: Martin Maechler, ETH Zurich
+  if(length(d <- dim(A)) != 2) stop("'A' is not a matrix") # <- also for sparseMatrix
+  # HMM constant: m <- c(1344,1344)
+  stopifnot(length(v) == (n <- d[1]), m.max >= 2)
+  # HMM constant: n <- 1344
+  if(n <= 1) {
+    if(n == 1) return(exp(A*t)*v)
+    stop("nrow(A) must be >= 1")
+  }
+  m <- min(n, m.max)  
+  # HMM constant: m <- 30
+  gamma <- 0.9        # constant
+  delta <- 1.2        # constant
+  nA <- norm(A, "I")  # varies with Q
+  # Next line varies with Q and phy
+  if(nA <  1e-6) { ## rescaling, by MMaechler, needed for small norms
+    A <- A/nA
+    t <- t*nA
+    nA <- 1
+  }
+  rndoff <- nA * .Machine$double.eps # Varies with Q
+  
+  t_1 <- abs(t) # Varies with phy
+  sgn <- sign(t) # Varies with phy
+  t_now <- 0
+  s_error <- 0
+  k1 <- 2
+  mb <- m    # HMM constant: mb <- 30
+  xm <- 1/m  # HMM constant: xm <- 1/30
+  # Next line is constant for all tips in HMM: beta <- 2*sqrt(5)
+  beta <- sqrt(sum(v*v))# = norm(v) = |\ v ||
+  if(beta == 0) ## border case: v is all 0, and the result is too
+    return(v)
+  # Next line is constant for all tips in HMM: fact <- 6.74967950018045e+33
+  fact <- (((m+1)/exp(1))^(m+1))*sqrt(2*pi*(m+1))
+  myRound <- function(tt) {
+    s <- 10^(floor(log10(tt)) - 1)
+    ceiling(tt/s)*s
+  }
+  t_new <- myRound( (1/nA)*(fact*tol/(4*beta*nA))^xm )
+  # alt for HMM, varies with Q, phy and site
+  # t_new <- myRound( (1/nA)*(nA*beta)^(-1/30)*7.48584399202831 )
+  # alt constant for HMM tips, varies with Q
+  # t_new <- myRound( (nA)^(-31/30)*7.12126158103164 )
+  
+  V <- matrix(0, n, m+1)    #HMM init: V <- matrix(0,1344,31)
+  H <- matrix(0, m+2, m+2)  #HMM initt: H <- matrix(0,32,32)
+  # nstep <- n.rej <- 0L      #irrelevant
+  w <- v
+  # updated in loop:
+  # t_now, t_new, V, H, w, beta
+  # updated on loop break: 
+  # mb
+  while (t_now < t_1) {
+    # nstep <- nstep + 1L
+    t_step <- min(t_1 - t_now, t_new)
+    # if(verbose) cat(sprintf("while(t_now = %g < ..): nstep=%d, t_step=%g\n",
+    #                         t_now, nstep, t_step))
+    V[,1] <- (1/beta)*w
+    for (j in 1:m) {
+      p <- as.vector(A %*% V[,j])  ## as of commit ab3e84e, this %*% is ~82% of all work!
+      for (i in 1:j) {
+        H[i,j] <- s <- sum(V[,i] *  p)
+        p <- p - s * V[,i]
+      }
+      s <- sqrt(sum(p*p))
+      if (s < btol) {
+        k1 <- 0
+        mb <- j
+        t_step <- t_1 - t_now
+        break
+      }
+      H[j+1, j] <- s
+      V[, j+1] <- p / s
+    } ## j-loop complete
+    if (k1 != 0) {
+      H[m+2, m+1] <- 1
+      av <- A %*% V[, m+1]  ## as of commit ab3e84e, this %*% is just ~2.7% of all work
+      avnorm <- sqrt(sum(av * av))
+    }
+    i.rej <- 0L
+    while (i.rej <= mxrej) {
+      mx <- mb + k1; imx <- seq_len(mx) # = 1:mx
+      # if(verbose) cat(sprintf("	inner while: k1=%d -> mx=%d\n",
+      #                         k1, mx))
+      F <- internal_expm(sgn * t_step * H[imx,imx, drop=FALSE])
+      if (k1 == 0) {
+        err_loc <- btol
+        break
+      } else {
+        phi1 <- abs(beta * F[m+1,1])
+        phi2 <- abs(beta * F[m+2,1] * avnorm)
+        if(is.nan(phi1) || is.nan(phi2))
+          stop("NaN phi values; probably overflow in expm()")
+        if (phi1 > 10*phi2) {
+          err_loc <- phi2
+          xm <- 1/m
+        } else if (phi1 > phi2) {
+          err_loc <- (phi1 * phi2)/(phi1 - phi2)
+          xm <- 1/m
+        } else {
+          err_loc <- phi1
+          xm <- 1/(m-1)
+        }
+      }
+      if (err_loc <= delta * t_step*tol) break
+      else {
+        if (i.rej == mxrej)
+          stop(gettextf('The requested tolerance (tol=%g) is too small for mxrej=%d.',
+                        tol, mxrej))
+        t_step <- gamma * t_step * (t_step * tol / err_loc)^xm
+        s <- 10^(floor(log10(t_step))-1)
+        t_step <- s * ceiling(t_step / s)
+        i.rej <- i.rej + 1L
+      }
+    }## end{ while (i.rej < mx..) }
+    # n.rej <- n.rej + i.rej
+    mx <- mb + max(0, k1-1); imx <- seq_len(mx) # = 1:mx
+    w <- as.vector(V[, imx] %*% (beta*F[imx,1, drop=FALSE]))
+    beta <- sqrt(sum(w*w))
+    t_now <- t_now + t_step
+    t_new <- myRound(gamma * t_step * (t_step*tol/err_loc)^xm)
+    # err_loc <- max(err_loc, rndoff)
+    # s_error <- s_error + err_loc
+  }# end{ while }
+  return(w)
+}
 
 GetExpQt <- function(phy, Q, scale.factor, rates=NULL){
 
