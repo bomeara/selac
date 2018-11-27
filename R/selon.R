e0f70f17ef2fbaee9f575128b5ab2e9a2dc62c81
thej022214 <jbeaulieu@nimbios.org>
2018-11-27 15:14:30 -0600

    finishing integration of new code into main code

--- R/selon.R
+++ R/selon.R
@@ -882,15 +882,14 @@ OptimizeEdgeLengthsUCENew <- function(phy, pars.mat, site.pattern.data.list, nuc
     generations <- FindBranchGenerations(phy)
     data.array <- MakeDataArray(site.pattern.data.list=site.pattern.data.list, phy=phy, nstates=4, nsites.vector=nsites.vector)
     pars.array <- MakeParameterArray(nuc.optim.list=nuc.optim.list, pars.mat=pars.mat, nsites.vector=nsites.vector)
-    print(pars.array[[1]])
     are_we_there_yet <- 1
     iteration.number <- 1
     old.likelihood <- GetBranchLikeAcrossAllSites(p=phy$edge.length, edge.number=NULL, phy=phy, data.array=data.array, pars.array=pars.array, nuc.model=nuc.model, diploid=diploid, n.cores=n.cores, logspace=logspace)
     while (are_we_there_yet > tol && iteration.number < maxit) {
-        cat("         Round number",  iteration.number, "\n")
+        cat("                 Round number",  iteration.number, "\n")
         for(gen.index in 1:length(generations)){
             for(index in 1:length(generations[[gen.index]])){
-                cat("              Optimizing edge number",  generations[[gen.index]][index],"\n")
+                cat("                   Optimizing edge number",  generations[[gen.index]][index],"\n")
                 out <- optimize(GetBranchLikeAcrossAllSites, edge.number=generations[[gen.index]][index], phy=phy, data.array=data.array, pars.array=pars.array, nuc.model=nuc.model, diploid=TRUE, n.cores=n.cores, logspace=logspace, lower=1e-8, upper=10, maximum=FALSE, tol = tol)
                 phy$edge.length[which(phy$edge[,2]==generations[[gen.index]][index])] <- out$minimum
             }
