7e62b3b5153688ac661c8b48150df053949635cd
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2018-12-20 09:20:21 -0600

    altering selon sims to address updates in code

--- R/selacSim.R
+++ R/selacSim.R
@@ -379,7 +379,7 @@ SelonSimulator <- function(phy, pars, nuc.optim_array, nuc.model, diploid=TRUE){
         nuc.mutation.rates <- CreateNucleotideMutationMatrix(pars[7:length(pars)], model=nuc.model, base.freqs=base.freqs)
     }
     if(nuc.model == "UNREST") {
-        tmp <- CreateNucleotideMutationMatrixSpecial(x[4:length(x)])
+        tmp <- CreateNucleotideMutationMatrixSpecial(pars[4:length(pars)])
         base.freqs <- tmp$base.freqs
         nuc.mutation.rates <- tmp$nuc.mutation.rates
     }
