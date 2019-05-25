00dd0e1e3917a969d86f498ee0939409e83918cb
Jeremy Beaulieu <jeremybeaulieu@jeremys-mbp.attlocal.net>
2019-05-25 15:25:50 -0500

    Changed the way we calculate discrete gamma based on Yang. It was slightly off from phangorn. This has not been used as far as I know, as we have exclusively used Felsenstein's quadrature approach

--- tests/testthat/test_likelihood.R
+++ tests/testthat/test_likelihood.R
@@ -25,7 +25,7 @@ test_that("GTR+GAMMA_likelihood", {
 	codon.data <- chars[phy$tip.label,]
 	nuc.sites <- selac:::SitePattern(codon.data)
 	gtr_gamma <- selac:::GetLikelihoodNucleotideForManyCharGivenAllParams(log(c(.5,1,1,1,1,1)), nuc.sites, phy, nuc.model="GTR", include.gamma=TRUE, gamma.type="median", logspace=TRUE, ncats=4, verbose=FALSE, n.cores.by.gene.by.site=1)
-	comparison <- identical(round(gtr_gamma,3), -8205.87)
+	comparison <- identical(round(gtr_gamma,3), -8192.526)
 	expect_true(comparison)
 })
 
@@ -109,21 +109,21 @@ test_that("selac+GAMMA_likelihood_median", {
     phy <- drop.tip(tree, "Calb")
     yeast.gene <- read.dna("gene1Yeast.fasta", format="fasta")
     yeast.gene <- as.list(as.matrix(cbind(yeast.gene))[1:7,])
-    chars <- DNAbinToCodonNumeric(yeast.gene)
+    chars <- selac:::DNAbinToCodonNumeric(yeast.gene)
     codon.data <- chars[phy$tip.label,]
-    aa.data <- ConvertCodonNumericDataToAAData(codon.data, numcode=1)
-    aa.optim <- apply(aa.data[, -1], 2, GetMaxName) #starting values for all, final values for majrule
+    aa.data <- selac:::ConvertCodonNumericDataToAAData(codon.data, numcode=1)
+    aa.optim <- apply(aa.data[, -1], 2, selac:::GetMaxName) #starting values for all, final values for majrule
     aa.optim.full.list <- aa.optim
-    codon.freq.by.aa <- GetCodonFreqsByAA(codon.data[,-1], aa.optim, numcode=1)
-    codon.freq.by.gene <- GetCodonFreqsByGene(codon.data[,-1])
+    codon.freq.by.aa <- selac:::GetCodonFreqsByAA(codon.data[,-1], aa.optim, numcode=1)
+    codon.freq.by.gene <- selac:::GetCodonFreqsByGene(codon.data[,-1])
     aa.optim.frame.to.add <- matrix(c("optimal", aa.optim), 1, dim(codon.data)[2])
     colnames(aa.optim.frame.to.add) <- colnames(codon.data)
     codon.data <- rbind(codon.data, aa.optim.frame.to.add)
-    codon.data <- SitePattern(codon.data, includes.optimal.aa=TRUE)
+    codon.data <- selac:::SitePattern(codon.data, includes.optimal.aa=TRUE)
     aa.optim = codon.data$optimal.aa
-    codon.index.matrix = CreateCodonMutationMatrixIndex()
-    selac_gamma <- GetLikelihoodSAC_CodonForManyCharGivenAllParams(log(c(4*4e-7*.5*5e6, 1.829272, 0.101799, .25, .25, .25, rep(1,5), 5)), codon.data, phy, aa.optim_array=aa.optim, codon.freq.by.aa=codon.freq.by.aa, codon.freq.by.gene=codon.freq.by.gene, numcode=1, diploid=TRUE, aa.properties=NULL, volume.fixed.value=0.0003990333, nuc.model="GTR", codon.index.matrix, include.gamma=TRUE, gamma.type="median", ncats=4, k.levels=0, logspace=TRUE, verbose=FALSE)
-    comparison <- identical(round(selac_gamma, 3), -6999.538)
+    codon.index.matrix = selac:::CreateCodonMutationMatrixIndex()
+    selac_gamma <- selac:::GetLikelihoodSAC_CodonForManyCharGivenAllParams(log(c(4*4e-7*.5*5e6, 1.829272, 0.101799, .25, .25, .25, rep(1,5), 5)), codon.data, phy, aa.optim_array=aa.optim, codon.freq.by.aa=codon.freq.by.aa, codon.freq.by.gene=codon.freq.by.gene, numcode=1, diploid=TRUE, aa.properties=NULL, volume.fixed.value=0.0003990333, nuc.model="GTR", codon.index.matrix, include.gamma=TRUE, gamma.type="median", ncats=4, k.levels=0, logspace=TRUE, verbose=FALSE)
+    comparison <- identical(round(selac_gamma, 3), -7007.447)
     expect_true(comparison)
 })
 
