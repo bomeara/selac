2f6be042d0433a2cd6fb682d1e0f21b7912816b5
Michael Gilchrist <mikeg@utk.edu>
2019-06-23 00:11:47 -0400

    minor tweak to selon starting.vals

--- R/selon.R
+++ R/selon.R
@@ -1351,7 +1351,7 @@ SelonOptimize <- function(nuc.data.path, n.partitions=NULL, phy, edge.length="op
         selon.starting.vals <- matrix(0, max.restarts+1, 2)
         selon.starting.vals[,1] <- runif(n = max.restarts+1, min = (10^-10)*5e6, max = (10^-8)*5e6)
         #selon.starting.vals[,2] <- runif(n = max.restarts+1, min = 0.01, max = 10)
-        selon.starting.vals[,2] <- runif(n = max.restarts+1, min = 0.01, max = max.a2)
+        selon.starting.vals[,2] <- runif(n = max.restarts+1, min = 0.01, max = max.a2/2)
     }else{
         selon.starting.vals <- matrix(c(1e-10*5e6, 100),1,2)
         selon.starting.vals <- rbind(selon.starting.vals, selon.starting.vals)
