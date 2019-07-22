35632e92188e853a681882edfbbe48f36b0c7a8c
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2019-07-22 10:08:08 -0500

    Added new option to control the number of threads that data.table is allowed to use. As of now data.table is only used for gtr and selon models

--- R/selon.R
+++ R/selon.R
@@ -768,9 +768,8 @@ MakeDataArray <- function(site.pattern.data.list, phy, nsites.vector) {
     for(partition.index in 2:length(site.pattern.data.list)){
         site.pattern.data.frame <- cbind(site.pattern.data.frame, site.pattern.data.list[[partition.index]][,2:(nsites.vector[partition.index]+1)])
     }
-    #site.pattern.data.table <- as.data.table(site.pattern.data.frame[,-1])
-    #site.pattern.data.table <- as.data.table(site.pattern.data.frame[,-1])
-    site.pattern.data.table <- t(site.pattern.data.frame[,-1])
+    site.pattern.data.table <- as.data.table(site.pattern.data.frame[,-1])
+    site.pattern.data.table <- t(site.pattern.data.table)
     colnames(site.pattern.data.table) <- phy$tip.label
     return(site.pattern.data.table)
 }
